9078ed6f16d03307c6154dbbe81dabca81ad2608
ripon4521 <riponakondo4521@gmail.com>
2025-03-11 17:53:46 +0600

    inital coommit

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json())
 
 
 
+
 const { MongoClient, ServerApiVersion, ObjectId  } = require('mongodb');
 const uri = `mongodb+srv://rechargeDB:usAPIL8MCWvy4zY2@cluster0.xm8ksdz.mongodb.net/?retryWrites=true&w=majority`;
 // mongodb+srv://<username>:<password>@cluster0.b1mistq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
@@ -35,12 +36,7 @@ async function run() {
     // await client.connect();
 
     const ordersCollections = client.db('HarbalShopDB').collection("order");
-    // const testimonialCollection = client.db('boot_Camp').collection("testimonials");
-    // const allUsersCollection = client.db('boot_Camp').collection("users");
-    // const allAdmissionCollection = client.db('boot_Camp').collection("admission");
 
-
-    
 
     app.post('/orders/create', async(req, res) => {
         const data = req.body;
@@ -50,60 +46,6 @@ async function run() {
     })
    
 
-    app.get("/allOrderData" , async(req , res)=>{
-      const cursur = ordersCollections.find();
-      const result = await cursur.toArray();
-      res.send(result)
-    })
-   
-    const { ObjectId } = require('mongodb');
-
-    app.patch('/allOrderData/:_id', async (req, res) => {
-      const id = req.params._id;
-      const filter = { _id: new ObjectId(id) };
-      const update = req.body;
-    
-      try {
-        // Use $set to update specific fields in the document
-        const result = await ordersCollections.updateOne(filter, { $set: update });
-        res.send(result);
-      } catch (error) {
-        console.error('Error updating order:', error);
-        res.status(500).send({ message: 'Failed to update the order.' });
-      }
-    });
-    
-
-
-    // app.put("/update/:id" , async(req , res)=>{
-    //   const   id  = req.params.id;
-    //   const filter = {_id : new ObjectId(id)}
-    //   const option = {upsert: true}
-    //   const updateBrand = req.body;
-    //   console.log(updateBrand)
-    //   const brand ={
-    //     $set:{
-    //         name:updateBrand.name,
-    //         email:updateBrand.email ,
-    //         address:updateBrand.address ,
-    //         university:updateBrand.university,
-      
-    //     }
-    //   }
-    //   const result = await allUsersCollection.updateOne(filter,brand,option);
-    //   res.send(result)
-    // })
-
-    
-
-    app.delete("/allOrderData/:_id" , async(req , res)=>{
-      const   id  = req.params._id;
-      const queary = {_id : new ObjectId(id)}
-      const result = await ordersCollections.deleteOne(queary);
-      res.send(result)
-    })
-
-  
 
 
 
